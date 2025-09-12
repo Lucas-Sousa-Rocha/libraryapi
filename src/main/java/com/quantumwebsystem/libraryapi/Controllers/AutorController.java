962cb812fdbe016123211bcd7e1fb5b2331d5768
Lucas Sousa <lucas.rocha2021@outlook.com>
2025-09-12 16:16:51 -0300

    Criando controller autor, criando service para salvar o autor, e controller respondendo 201 CREATE, com resposta para a api com o objeto criado, e URL do objeto

--- src/main/java/com/quantumwebsystem/libraryapi/Controllers/AutorController.java
+++ src/main/java/com/quantumwebsystem/libraryapi/Controllers/AutorController.java
@@ -1,9 +1,32 @@
 package com.quantumwebsystem.libraryapi.Controllers;
 
+
+import com.quantumwebsystem.libraryapi.Controllers.DTO.AutorDTO;
+import com.quantumwebsystem.libraryapi.Model.Autor;
+import com.quantumwebsystem.libraryapi.Service.AutorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import java.net.URI;
 
 @RestController
-@RequestMapping("/api/autor")
+@RequestMapping("/api/autores")
 public class AutorController {
+
+    private final AutorService autorService;
+
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AutorDTO> salvarAutor(@RequestBody AutorDTO autorDTO) {
+        AutorDTO autorDto = autorService.salvarAutor(autorDTO);
+        URI uri = URI.create("/api/autores/" + autorDto.getId());
+        return ResponseEntity.created(uri).body(autorDto);
+    }
+
 }
---
+++ src/main/java/com/quantumwebsystem/libraryapi/Service/AutorService.java
@@ -0,0 +1,31 @@
+package com.quantumwebsystem.libraryapi.Service;
+
+import com.quantumwebsystem.libraryapi.Controllers.DTO.AutorDTO;
+import com.quantumwebsystem.libraryapi.Model.Autor;
+import com.quantumwebsystem.libraryapi.Repository.AutorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutorService {
+
+    private final AutorRepository autorRepository;
+
+    public AutorService(AutorRepository autorRepository) {
+        this.autorRepository = autorRepository;
+    }
+
+
+    public AutorDTO salvarAutor(AutorDTO autorDTO){
+        Autor autor = new Autor();
+        autor.setNome(autorDTO.getNome());
+        autor.setNacionalidade(autorDTO.getNacionalidade());
+        autor.setDt_nascimento(autorDTO.getDt_nascimento());
+        var autorsave = autorRepository.save(autor);
+
+        AutorDTO resposta = new AutorDTO();
+        resposta.setId(autorsave.getId());
+        resposta.setNome(autorsave.getNome());
+        resposta.setNacionalidade(autorsave.getNacionalidade());
+        return resposta;
+    }
+}
