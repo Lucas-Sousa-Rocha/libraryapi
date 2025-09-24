34bc9c4a814e07c16bbb38f61e7897d6ee7d2e83
Lucas Sousa <lucas.rocha2021@outlook.com>
2025-09-24 16:17:16 -0300

    feat: adicionando a busca do livro por nome do autor, genero, ano de publicação, apenas passando a data de publicação ex '1984', assim trazendo uma busca mais flexivel

--- src/main/java/com/quantumwebsystem/libraryapi/Repository/Specs/LivroSpecs.java
+++ src/main/java/com/quantumwebsystem/libraryapi/Repository/Specs/LivroSpecs.java
@@ -3,18 +3,68 @@ package com.quantumwebsystem.libraryapi.Repository.Specs;
 import com.quantumwebsystem.libraryapi.Model.GeneroLivro;
 import com.quantumwebsystem.libraryapi.Model.Livro;
 import org.springframework.data.jpa.domain.Specification;
+import java.time.LocalDate;
 
 public class LivroSpecs {
 
-    public static Specification<Livro> isbnEquals(String isbn){
+    public static Specification<Livro> isbnEquals(String isbn) {
         return (root, query, cb) -> cb.equal(root.get("isbn"), isbn);
     }
 
-    public static Specification<Livro> tituloLike(String titulo){
-        return (root, query, cb) -> cb.equal(cb.upper(root.get("titulo")), "%"+titulo.toUpperCase()+"%");
+    public static Specification<Livro> tituloLike(String titulo) {
+        return (root, query, cb) -> cb.like(cb.lower(root.get("titulo")), "%" + titulo.toLowerCase() + "%");
     }
 
-    public static Specification<Livro> generoIquals(GeneroLivro  genero){
+    public static Specification<Livro> autorNomeLike(String nomeAutor) {
+        return (root, query, cb) -> cb.like(cb.lower(root.join("autor").get("nome")), "%" + nomeAutor.toLowerCase() + "%");
+    }
+
+    public static Specification<Livro> generoEquals(GeneroLivro genero) {
         return (root, query, cb) -> cb.equal(root.get("genero"), genero);
     }
+
+    public static Specification<Livro> anoPublicacaoEquals(Integer anoPublicacao) {
+        return (root, query, cb) -> cb.equal(cb.function("to_char", String.class, root.get("dtPublicacao"), cb.literal("YYYY")),anoPublicacao.toString());
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/quantumwebsystem/libraryapi/Service/LivroService.java
+++ src/main/java/com/quantumwebsystem/libraryapi/Service/LivroService.java
@@ -36,23 +36,30 @@ public class LivroService {
         livroRepository.delete(livro);
     }
 
-    public List<Livro> pesquisa(String isbn, String titulo, String nomeAutor, GeneroLivro  genero, Integer anoPublicacao){
+    public List<Livro> pesquisa(String isbn, String titulo, String nomeAutor, GeneroLivro genero, Integer anoPublicacao) {
+        Specification<Livro> livroSpecification = Specification.allOf();
 
-        Specification<Livro> livroSpecification = (root, query, cb) -> cb.conjunction();
-
-        if (isbn != null) {
+        if (isbn != null && !isbn.isBlank()) {
             livroSpecification = livroSpecification.and(LivroSpecs.isbnEquals(isbn));
         }
-        if (titulo != null) {
+        if (titulo != null && !titulo.isBlank()) {
             livroSpecification = livroSpecification.and(LivroSpecs.tituloLike(titulo));
         }
+        if (nomeAutor != null && !nomeAutor.isBlank()) {
+            livroSpecification = livroSpecification.and(LivroSpecs.autorNomeLike(nomeAutor));
+        }
         if (genero != null) {
-            livroSpecification = livroSpecification.and(LivroSpecs.generoIquals(genero));
+            livroSpecification = livroSpecification.and(LivroSpecs.generoEquals(genero));
+        }
+        if (anoPublicacao != null) {
+            livroSpecification = livroSpecification.and(LivroSpecs.anoPublicacaoEquals(anoPublicacao));
         }
 
-        List<Livro> livros = livroRepository.findAll(livroSpecification);
-        return livroRepository.findAll(LivroSpecs.isbnEquals(isbn));
+        return livroRepository.findAll(livroSpecification);
     }
+
+
+
 
 }
 
